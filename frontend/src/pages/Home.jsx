ec5a1b49c06d6b374254e519e855707c00b9aaaa
tanmay-8 <tanmayshingde8@gmail.com>
2024-08-12 23:32:26 +0530

    done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FlashCardSets from "../components/FlashCardSets";
 
 const Home = () => {
+    const [isLogin, setIsLogin] = useState(
+        localStorage.getItem("token") ? true : false
+    );
+
+    useEffect(() => {
+        setIsLogin(localStorage.getItem("token") ? true : false);
+
+        if (!isLogin) {
+            window.location.href = "/login";
+        }
+    }, []);
+
     return (
         <div className="p-8 py-10 mx-auto">
             <FlashCardSets />
